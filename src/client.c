2e67c8dba65e49699f4edc480aa204c4f1f63f59
dhsudev <lua@gnomo.net>
2024-08-08 15:34:53 +0200

    Implemented func to send signals in client, and send_lenght

--- src/client.c
+++ src/client.c
@@ -1,45 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltrevin- <ltrevin-@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 15:32:04 by ltrevin-          #+#    #+#             */
+/*   Updated: 2024/08/08 15:34:48 by ltrevin-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <minitalk.h>
 
-void send_bit(int pid, int bit)
+void	send_bit(int pid, int bit)
 {
-	int sent;
+	int	sent;
 
-	if(bit == 1)
+	if (bit == 1)
 		sent = kill(pid, SIGUSR1);
-	else 
+	else
 		sent = kill(pid, SIGUSR2);
-	if(sent != 0)
+	if (sent != 0)
 	{
-		ft_printf("ERROR: unable to send a signal to the process, check the PID and if the server is still running please");
+		ft_printf("ERROR: unable to send a signal to the process,");
+		ft_printf("check the PID and if the server is still running please");
 		exit(1);
 	}
 	ft_printf("Sent %d \n", bit);
 	usleep(20);
 }
 
-void send_char(int pid, char ch)
+void	send_char(int pid, char ch)
 {
 	ft_printf("Debug: Sent '%c' to server [%d]\n", ch, pid);
 }
 
-int send_lenght(int pid, int len)
+int	send_lenght(int pid, int len)
 {
-	int bits; 
-	bits = sizeof(int) * 8;
+	int	bits;
 
+	bits = sizeof(int) * 8;
 	ft_printf("Debug: send_lenght(%d, %d) called\n", pid, len);
 	while (bits--)
 		send_bit(pid, (len >> bits) & 1);
 	return (0);
 }
 
-void send_message(int pid, char *message)
+void	send_message(int pid, char *message)
 {
-	int i;
+	int	i;
 
 	send_lenght(pid, ft_strlen(message));
 	i = 0;
-	while(message[i])
+	while (message[i])
 		send_char(pid, message[i++]);
 	send_char(pid, '\0');
 }
@@ -51,19 +64,20 @@ int	main(int argc, char **argv)
 	if (argc == 3)
 	{
 		srv_pid = ft_atoi(argv[1]);
-		if(!srv_pid)
+		if (!srv_pid)
 		{
 			ft_printf("ERROR: Wrong PID, check the PID please");
 			return (1);
 		}
-		else 
+		else
 		{
 			send_message(srv_pid, argv[2]);
 		}
 	}
 	else
 	{
-		ft_printf("ERROR: Invalid arguments, correct syntax is:\n\n\t./client [server PID] [message]\n\n");
+		ft_printf("ERROR: Invalid arguments, correct syntax is:\n");
+		ft_printf("\n\t./client [server PID] [message]\n\n");
 		return (1);
 	}
 	return (0);
--- src/server.c
+++ src/server.c
@@ -1,11 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltrevin- <ltrevin-@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 15:32:07 by ltrevin-          #+#    #+#             */
+/*   Updated: 2024/08/08 15:32:55 by ltrevin-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <minitalk.h>
 
-
-int main(void)
+int	main(void)
 {
 	ft_printf("Welcome to lua's minitalk! 🦦🔅\n");
 	ft_printf("\tPID: %d\n", getpid());
-	while(1)
-		pause(); 
+	while (1)
+		pause();
 }
-
